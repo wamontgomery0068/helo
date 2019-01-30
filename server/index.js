1a1d5b142d003bd9c2edad6527361a4153c2a8cc
Your Name <wamontgomery0068@gmail.com>
2019-01-30 16:44:24 -0600

    Set Up massive

--- server/index.js
+++ server/index.js
@@ -9,12 +9,9 @@ app.use(json());
 
 const port = 3056;
 
-
-
-
-
-
-
+massive( process.env.CONNECTION_STRING ).then(db => {
+    app.set('db', db)
+}).catch( err => console.log(err) );
 
 
 app.listen(port, () => console.log(`Listening on ${port}`));
