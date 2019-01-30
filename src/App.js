b0d0ec03e85da0aaa6878a348dc1e884dad8d664
Your Name <wamontgomery0068@gmail.com>
2019-01-30 16:15:43 -0600

    Render Components In App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Imported Components Below
+
+import Nav from './component/Nav/Nav';
+import Auth from './component/Auth/Auth';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Post from './component/Post/Post';
+
+// Imported Components Above
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <Auth />
+        <Dashboard />
+        <Form />
+        <Post />
       </div>
     );
   }
