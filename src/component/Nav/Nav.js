eff2d7987c16239d407632a52fa987d0aa9ceabe
Your Name <wamontgomery0068@gmail.com>
2019-01-30 16:09:17 -0600

    Created a functional component

---
+++ src/component/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Nav() {
+    return (
+        <div className = "Nav_Container">
+            <h1> Nav Component </h1>
+        </div>
+    )
+};
