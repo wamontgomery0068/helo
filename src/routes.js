8cc48c9de6858c2cea7f389883b721a058c267ab
Your Name <wamontgomery0068@gmail.com>
2019-01-30 20:21:45 -0600

    imported

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+// Imported Components Below
+
+import Auth from './component/Auth/Auth';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Post from './component/Post/Post';
+
+// Import Components Above
+
+export default (
+    <Switch>
+        <Route component = {Auth} exact path = "/" />
+        <Route component = {Dashboard} path = "/dashboard" />
+        <Route component = {Post} path = "/post/:postid" />
+        <Route component = {Form} path = "/new"/>
+    </Switch>
+);
