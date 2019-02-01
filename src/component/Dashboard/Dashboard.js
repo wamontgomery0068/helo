202572d922c76270b4d20ba133c9fb87dae1449f
Your Name <wamontgomery0068@gmail.com>
2019-02-01 16:23:33 -0600

    Working on Styling

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -11,7 +11,17 @@ class Dashboard extends Component {
     render() {
         return (
             <div className = "Dashboard_Container">
-            
+                <div className = "Dashboard_Search">
+                    <div className = "Search_Input"></div>
+                    <button className = "Search_Button"></button>
+                    <button className = "Reset_Button">
+                        <p className = "Reset_Text"> Reset </p>
+                    </button>
+                    <div className = "My_Post_Text"></div>
+                    <input className = "Input_Checkbox"></input>
+                </div>
+                <div className = "Dashboard_Entry"></div>
+        
             </div>
         );
     }
