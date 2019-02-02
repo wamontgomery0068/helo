a44b722f914957866427c778d1158aefbc642e71
Your Name <wamontgomery0068@gmail.com>
2019-02-01 22:49:54 -0600

    Styling

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Search from "../../Images/search.png";
 import './Dashboard.css'
 
 class Dashboard extends Component {
@@ -12,15 +13,25 @@ class Dashboard extends Component {
         return (
             <div className = "Dashboard_Container">
                 <div className = "Dashboard_Search">
-                    <div className = "Search_Input"></div>
-                    <button className = "Search_Button"></button>
+                    <input className = "Search_Input" type = "text" placeholder = "Search by Title" />
+                    <button className = "Search_Button">
+                        <img className = "Search_Image" src = { Search } />
+                    </button>
                     <button className = "Reset_Button">
                         <p className = "Reset_Text"> Reset </p>
                     </button>
-                    <div className = "My_Post_Text"></div>
-                    <input className = "Input_Checkbox"></input>
+                    <div className = "My_Post">
+                        <p className = "My_Post_Text"> My Posts </p>
+                    </div>
+                    <input className = "Input_Checkbox" type = "Checkbox" />
                 </div>
-                <div className = "Dashboard_Entry"></div>
+                <div className = "Dashboard_Entry">
+                    <div className = "Data_Entry">
+                        <div className = "Data_Name"></div>
+                        <div className = "Data_Author"></div>
+                        <div className = "Data_Profile_Image"></div>
+                    </div>
+                </div>
         
             </div>
         );
