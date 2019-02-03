201e083b294725d6b0e83e07f5135ea2b002ebab
Your Name <wamontgomery0068@gmail.com>
2019-02-03 14:42:37 -0600

    Took out user profile image

--- db/helo_user table.sql
+++ db/helo_user table.sql
@@ -2,6 +2,5 @@ CREATE TABLE helo_user
 (
   id SERIAL PRIMARY KEY,
   username VARCHAR (100) UNIQUE NOT NULL,
-  password VARCHAR (100) NOT NULL,
-  profile_picture VARCHAR (500)
+  password VARCHAR (100) NOT NULL
 );
