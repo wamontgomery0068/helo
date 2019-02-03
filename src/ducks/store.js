c8b54398720dd6de30308b15b18639efa0cb4e2b
Your Name <wamontgomery0068@gmail.com>
2019-02-03 15:15:20 -0600

    Removed the word Compone

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import promiseMiddleWare from "redux-promise-middleware";
 
 import reducer from "./reducer";
