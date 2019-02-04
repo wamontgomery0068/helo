a91d780aafc19a159fe2942b2f2bf9fbbc5adc57
Your Name <wamontgomery0068@gmail.com>
2019-02-04 10:21:43 -0600

    Put in the Functionality. Need to test.

--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import {Redirect} from "react-router-dom";
 import './Auth.css';
 import Wink from "../../Images/winkface.png";
+
+// Imports from the Ducks Folder Below
+
+import {login} from '../../ducks/reducer';
+import {register} from '../../ducks/reducer';
+
+// Imports from the Ducks Folder Above
 
 class Auth extends Component {
     constructor(){
@@ -10,9 +19,29 @@ class Auth extends Component {
             username: "",
             password: ""
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
+    };
+
+    handleChange(e){
+        this.setState({ [e.target.name]: e.target.value})
+    };
+
+    handleLogin(e){
+        e.preventDefault();
+        this.props.login(this.state.username, this.state.password)
+    };
+
+    handleRegister(e){
+        e.preventDefault();
+        this.props.Register(this.state.username, this.state.password)
     };
 
     render() {
+        if (this.props.reducer.username){
+            return <Redirect to = '/dashboard' />
+        };
         return (
             <div className = "Auth_Container">
                 <div className = "Auth_Card">
@@ -23,16 +52,54 @@ class Auth extends Component {
                         <p className = "Auth_Card_Title_Text"> Helo </p>
                     </div>
                     <div className = "Auth_Card_Field">
-                        <p className = "Auth_Card_Field_Text"> Username: <input className = "Auth_Input_1" /></p>
-                        <p className = "Auth_Card_Field_Text"> Password: <input className = "Auth_Input_2" /> </p>
+
+                        <p className = "Auth_Card_Field_Text">
+                             Username: 
+                                <input 
+                                    onChange = {this.handleChange}
+                                    value = {this.state.username}
+                                    name = "username"
+                                    placeholder = "Username"
+                                    className = "Auth_Input_1" 
+                                />
+                        </p>
+
+                        <p className = "Auth_Card_Field_Text">
+                             Password: 
+                                <input 
+                                    onChange = {this.handleChange}
+                                    value = {this.state.password}
+                                    name = "password"
+                                    placeholder = "Password"
+                                    className = "Auth_Input_2" 
+                                />
+                        </p>
+
                     </div>
                     <div className = "Auth_Card_Buttons">
-                        <button className = "Auth_Card_Button_Login">
-                            <p className = "Auth_Card_Button_Login_Text"> Login </p>
+
+                        <button 
+                            className = "Auth_Card_Button_Login" 
+                            onClick = {this.handleLogin}
+                        >
+                            <p 
+                                className = "Auth_Card_Button_Login_Text"
+                            > Login 
+                            </p>
+
                         </button>
-                        <button className = "Auth_Card_Button_Register">
-                            <p className = "Auth_Card_Button_Register_Text"> Register </p>
+
+                        <button 
+                            className = "Auth_Card_Button_Register" 
+                            onClick = {this.handleRegister}
+                        >
+                            <p 
+                                className = "Auth_Card_Button_Register_Text"
+                            > Register 
+                            </p>
+
                         </button>
+
                     </div>
                 </div>
             </div>
@@ -40,4 +107,10 @@ class Auth extends Component {
     }
 };
 
-export default Auth;
+const mapStateToProps = (state) => {
+    return {
+        reducer: state.reducer
+    };
+};
+
+export default connect (mapStateToProps, {login: login}, {register: register}) (Auth);
