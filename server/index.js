e19d1606194be1d9699493d5719aa250a4d7c255
Your Name <wamontgomery0068@gmail.com>
2019-02-04 08:32:51 -0600

    Database is connected

--- server/index.js
+++ server/index.js
@@ -1,17 +1,28 @@
 require("dotenv").config();
-const { json } = require("body-parser");
 const express = require("express");
+const {json} = require("body-parser");
 const massive = require("massive");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
 const app = express();
-
+const ac = require("./controller/authController");
 
 app.use(json());
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
 
-const port = 3056;
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set("db", db);
+    console.log("Your database is connected!")
+})
 
-massive( process.env.CONNECTION_STRING ).then(db => {
-    app.set('db', db)
-}).catch( err => console.log(err) );
 
-
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(process.env.EXPRESS_PORT || 3056, () => {
+    console.log(`Listening on ${process.env.EXPRESS_PORT}`);
+});
